7c58b36150db7aeb6233b0d146005bf8fb189d4d
Osaro Imohe <osaroimohe99@yahoo.com>
2022-03-10 19:08:51 +0100

    [Server] Remove vestige db connection string

--- server/main.go
+++ server/main.go
@@ -4,13 +4,9 @@ import (
 	"os"
 
 	"github.com/osaro-imohe/ping-pong/server/app"
-	"github.com/osaro-imohe/ping-pong/server/config"
 )
 
 func main() {
-	config := config.GetConfig()
-
 	app := &app.App{}
-	app.Initialize(config)
 	app.Run(":" + os.Getenv("PORT"))
 }
